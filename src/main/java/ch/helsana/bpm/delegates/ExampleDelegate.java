d382a25e92830a5f22e533c6e82d24fadbace914
Michael Würsch <wuersch@me.com>
2019-04-12 10:30:44 +0200

    playing around with @injected

--- src/main/java/ch/helsana/bpm/delegates/ExampleDelegate.java
+++ src/main/java/ch/helsana/bpm/delegates/ExampleDelegate.java
@@ -1,10 +1,13 @@
 package ch.helsana.bpm.delegates;
 
+import javax.inject.Named;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 
+@Named("CreateTweetDelegate")
 public class ExampleDelegate implements JavaDelegate {
 	// private final Logger LOGGER = LoggerFactory.getLogger(ExampleDelegate.class.getName());
 
