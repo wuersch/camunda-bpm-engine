719862e9aacae0860991ed46563d789010914c0f
Michael Würsch <wuersch@me.com>
2019-04-12 10:12:56 +0200

    minimal delegate for testing

--- src/main/java/ch/helsana/bpm/delegates/ExampleDelegate.java
+++ src/main/java/ch/helsana/bpm/delegates/ExampleDelegate.java
@@ -2,14 +2,14 @@ package ch.helsana.bpm.delegates;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 public class ExampleDelegate implements JavaDelegate {
-	private final Logger LOGGER = LoggerFactory.getLogger(ExampleDelegate.class.getName());
+	// private final Logger LOGGER = LoggerFactory.getLogger(ExampleDelegate.class.getName());
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		LOGGER.info("Invocation of " + ExampleDelegate.class.getName());
+		// LOGGER.info("Invocation of " + ExampleDelegate.class.getName());
 	}
 }
