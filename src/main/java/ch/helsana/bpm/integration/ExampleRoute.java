8b47d0af0ab8302374227126a71f238c0a20fba4
Michael Würsch <wuersch@me.com>
2019-04-18 23:14:23 +0200

    added json marshalling

--- src/main/java/ch/helsana/bpm/integration/ExampleRoute.java
+++ src/main/java/ch/helsana/bpm/integration/ExampleRoute.java
@@ -2,6 +2,7 @@ package ch.helsana.bpm.integration;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,7 @@ public class ExampleRoute extends RouteBuilder {
             .bean("GetBankRequestBuilder")
             .setHeader(CxfConstants.OPERATION_NAME, constant("getBank"))
             .to("cxf://http://www.thomas-bayer.com/axis2/services/BLZService?serviceClass=com.thomas_bayer.blz.BLZServicePortType&wsdlURL=/wsdl/BLZService.wsdl")
+            .marshal().json(JsonLibrary.Gson)
             //.to("mongodb:mongoClient?database=camunda&collection=partners&operation=insert")
             .log("The response was ${body[0]}");
             ;
