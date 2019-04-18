2520cb0e670d47cb93367f6afeb55d5a48f1868c
Michael Würsch <wuersch@me.com>
2019-04-18 22:21:21 +0200

    another try on beans

--- src/main/java/ch/helsana/bpm/integration/AddRequestBuilder.java
+++ src/main/java/ch/helsana/bpm/integration/AddRequestBuilder.java
@@ -1,13 +1,9 @@
 package ch.helsana.bpm.integration;
 
-import org.tempuri.Add;
+import org.apache.camel.Exchange;
 
 public class AddRequestBuilder {
-    public Add build(String todo) {
-        Add request = new Add();
-        request.setIntA(2);
-        request.setIntB(3);
-
-        return request;
+    public void build(Exchange exchange) {
+        exchange.getOut().setBody(new Object[] {1 , 2});
     }
 }
