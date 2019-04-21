0b6910f5106fbf4fd58dbdd6e4b1b4f07315c754
Michael Würsch <wuersch@me.com>
2019-04-21 11:44:16 +0200

    trying json conversion

--- src/main/java/ch/helsana/bpm/integration/DbObjectBuilder.java
+++ src/main/java/ch/helsana/bpm/integration/DbObjectBuilder.java
@@ -4,11 +4,18 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component("DbObjectBuilder")
 public class DbObjectBuilder {
+    private final Logger LOGGER = LoggerFactory.getLogger(DbObjectBuilder.class.getName());
+
     public void build(String body, Exchange exchange) {
-        DBObject dbObject = (DBObject) BasicDBObject.parse(body);
+        LOGGER.info("Body before: " + body);
+        String json = body.replace('[', '{').replace(']', '}');
+        LOGGER.info("Body after: " + body);
+        DBObject dbObject = (DBObject) BasicDBObject.parse(json);
         exchange.getOut().setBody(dbObject);
     }
 }
