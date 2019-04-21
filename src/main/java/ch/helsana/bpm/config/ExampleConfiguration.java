560aa531f111b2b1f46b941f142ecb67c6453e53
Michael Würsch <wuersch@me.com>
2019-04-21 17:09:48 +0200

    added auth to mongdb

--- src/main/java/ch/helsana/bpm/config/ExampleConfiguration.java
+++ src/main/java/ch/helsana/bpm/config/ExampleConfiguration.java
@@ -1,6 +1,12 @@
 package ch.helsana.bpm.config;
 
-import com.mongodb.MongoClient;
+import java.util.Arrays;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +20,27 @@ public class ExampleConfiguration {
     @Value("${ods.mongodb.port}")
     private int mongoDbPort;
 
+    @Value("${ods.mongodb.db}")
+    private String db;
+
+    @Value("${ods.mongodb.username}")
+    private String username;
+
+    @Value("${ods.mongodb.password}")
+    private String password;
+
     @Bean
     MongoClient mongoClient() {
-        return new MongoClient(mongoDbHost, mongoDbPort);
+        MongoCredential credential = MongoCredential.createCredential(username, db, password.toCharArray());
+        
+        MongoClient mongoClient = MongoClients.create(
+            MongoClientSettings.builder()
+                               .applyToClusterSettings(builder -> 
+                                    builder.hosts(Arrays.asList(new ServerAddress("host1", 27017)))
+                                )
+                               .credential(credential)
+                               .build());  
+        
+        return mongoClient;  
     }
 }
