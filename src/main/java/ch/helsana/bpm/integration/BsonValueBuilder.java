27ef93656d65045eabdc0917cd6dbb7ef248a3c1
Michael Würsch <wuersch@me.com>
2019-04-24 21:20:30 +0200

    fixes

--- src/main/java/ch/helsana/bpm/integration/BsonValueBuilder.java
+++ src/main/java/ch/helsana/bpm/integration/BsonValueBuilder.java
@@ -1,12 +1,11 @@
 package ch.helsana.bpm.integration;
 
-import org.bson.BsonString;
-import org.bson.BsonValue;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Component("BsonValueBuilder")
 public class BsonValueBuilder {
-    public BsonValue build(String body) {
-        return new BsonString(body);
+    public ObjectId build(String body) {
+        return new ObjectId(body);
     }
 }
